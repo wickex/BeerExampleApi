59753190437eb3ce489d6c2aa4cb11e7ec1c05a1
Wickex <wickex@users.noreply.github.com>
2021-03-24 19:43:02 +0100

    Add simple beer model

---
+++ BeerExampleApi/Models/Beer.cs
@@ -0,0 +1,9 @@
+﻿namespace BeerExampleApi.Models
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Percentage { get; set; }
+    }
+}
