40fac69a9c0122e528e63b1cf7f2b52323d0aee2
Wickex <wickex@users.noreply.github.com>
2021-03-24 19:52:16 +0100

    Add EF Core

---
+++ BeerExampleApi/BeerContext.cs
@@ -0,0 +1,14 @@
+﻿using BeerExampleApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerExampleApi
+{
+    public class BeerContext : DbContext
+    {
+        public BeerContext(DbContextOptions<BeerContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Beer> Beers { get; set; }
+    }
+}
